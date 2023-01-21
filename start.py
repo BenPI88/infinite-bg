b1b1d4e2eee7db037e896dc7d3c17e5124f135f7
Benji <97198967+BenPI88@users.noreply.github.com>
2023-01-21 11:43:29 -0600

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 import os
 import time
 os.system("sudo apt-get install ImageMagick")
-os.system("import /.bg.png")
-os.system("gsettings get org.gnome.desktop.background picture-uri 'file:///.bg.png'")
+while True:
+	os.system("import /.bg.png")
+	os.system("gsettings get org.gnome.desktop.background picture-uri 'file:///.bg.png'")
